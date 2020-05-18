64672d56049137bb72d440b14448ac1454244bb7
Jim Wang <jim.z.wang8@gmail.com>
2020-05-18 16:09:11 -0700

    Clion

---
+++ cmake-build-debug/CMakeFiles/PEX_4_CHWA_WANG.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PEX_4_CHWA_WANG.dir/main.c.o"
+  "PEX_4_CHWA_WANG.exe"
+  "PEX_4_CHWA_WANG.pdb"
+  "libPEX_4_CHWA_WANG.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PEX_4_CHWA_WANG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
